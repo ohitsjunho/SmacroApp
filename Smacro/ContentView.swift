b922c059fec84c07899a5921feee035f7aea74c3
SalTheSheep <ohjunho725@gmail.com>
2024-06-20 13:40:14 -0400

    Initial Commit

---
+++ Smacro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smacro
+//
+//  Created by Junho Oh on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smacro/SmacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmacroApp.swift
+//  Smacro
+//
+//  Created by Junho Oh on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SmacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
