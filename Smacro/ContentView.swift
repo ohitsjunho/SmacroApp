e3138063638df77d8cecba4e912166626b779ee0
SalTheSheep <ohjunho725@gmail.com>
2024-06-20 14:10:20 -0400

    Initial Project Creation of Smacro

--- Smacro/ContentView.swift
+++ Smacro/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemOrange)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 5.0) {
+                Image("macropie")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                HStack {
+                    Image("beef")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+                    Image("milk")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+                    Image("chickenbreast")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                Text("Smacro Count")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("Protein:")
+                Text("Carbohydrates:")
+                Text("Fats:")
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white).cornerRadius(10).shadow(radius: 15))
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
