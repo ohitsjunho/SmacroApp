66da0ef705bf0101827a57eadebbcb37817a8f90
jasonmauro <jamauro619@gmail.com>
2024-06-21 01:21:16 -0400

    popup window

--- Smacro/InputProtein.swift
+++ Smacro/InputProtein.swift
@@ -5,4 +5,55 @@
 //  Created by Jason Mauro on 6/21/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct InputProtein: View{
+    let title: String
+    
+    
+    
+    
+    var body: some View {
+        
+        @State var input: String = ""
+        
+        ZStack{
+            
+            Color(.systemOrange).ignoresSafeArea()
+            
+            
+            VStack(spacing : 0.1){
+                
+                Text(title)
+                    .font(.title)
+                    .bold()
+                    .padding()
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.white)
+                    
+                    TextField("Protein: ", text: $input)
+                        .padding()
+                        .keyboardType(.decimalPad)
+                        .background(.white)
+                }
+                
+                
+                
+            }
+            .background(.gray)
+            .cornerRadius(10)
+            .fixedSize(horizontal: false, vertical: true)
+            .padding();
+            
+        }
+    }
+    
+}
+
+struct InputProtein_Preivew: PreviewProvider{
+    static var previews: some View{
+        InputProtein(title: "Input Protein (grams)")
+    }
+}
